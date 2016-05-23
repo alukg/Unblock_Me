602c4c92f6e9bb4f3f843ac5bcab02a4d0e09d53
Yaniv Daye <yaniv120892@gmail.com>
2016-05-23 23:31:12 +0300

    add classes block,board,game and test

---
+++ src/GameMenu/Block.java
@@ -0,0 +1,95 @@
+package GameMenu;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+public class Block extends JLabel 
+{
+	private int x;
+	private int y;
+	private int length;
+	private String dir;
+	private Boolean target;//Saves whether the block is a target block or not.
+	private Boolean Selected;
+	
+	
+	
+	//Constructors
+	public Block(int x, int y, int length, String dir, Boolean target)
+	{
+		super();
+		this.target = target;
+		this.dir = dir;
+		this.x = x;
+		this.y = y;
+		this.length = length;
+		this.Selected = false;
+	}
+	public Block(Block b)
+	{
+		 super();
+		 this.x = b.getX();
+		 this.y = b.getY();
+		 this.length = b.getLength();
+		 this.dir = b.getDir();
+		 this.target = b.getTarget();
+		 this.Selected = false;
+	}
+	
+	
+	//Getters and Setters
+	public Boolean getSelected() {
+		return Selected;
+	}
+	public void setSelected(Boolean selected) {
+		Selected = selected;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getLength() {
+		return length;
+	}
+	public String getDir() {
+		return dir;
+	}
+	public Boolean getTarget() {
+		return target;
+	}
+	
+	public void moveRight()
+	{
+		this.x = this.x + 1;
+	}
+	public void moveLeft()
+	{
+		this.x = this.x - 1;
+	}
+	public void moveUp()
+	{
+		this.y = this.y + 1;
+	}
+	public void moveDown()
+	{
+		this.y = this.y - 1;
+	}
+	
+	
+
+}
---
+++ src/GameMenu/Board.java
@@ -0,0 +1,424 @@
+package GameMenu;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Board extends JPanel implements MouseListener, KeyListener
+{
+	private Block selected;
+	private Block[] allBlocks;
+	private Game game;
+	private int[][] arrBoard;//free 0 , occupied 1
+	private int size;
+	private final int finishx = 3;
+	private final int finishy = 3;
+	
+	
+	
+	public int getFinishx() {
+		return finishx;
+	}
+	public int getFinishy() {
+		return finishy;
+	}
+
+	public Board(Board b)
+	{
+		super();
+		this.game = b.getGame();
+		this.size = b.getBoardSize();
+		this.selected = b.getSelected();
+		this.setLayout(new GridBagLayout());
+		this.setSize(100, 100);
+		this.setBackground(Color.yellow);
+		this.allBlocks = new Block[b.getAllBlocks().length];
+		for (int i = 0; i < allBlocks.length; i++) 
+		{
+			this.allBlocks[i] = new Block(b.getAllBlocks()[i]);
+		}
+		CreateFrame(this.size, this.allBlocks);
+		AddBlocks(this.allBlocks);
+		//this.setBorder(BorderFactory.createTitledBorder(
+		//        BorderFactory.createEtchedBorder(), "Board"));
+		this.setVisible(true);
+		this.addKeyListener(this);
+	}
+	
+	public Game getGame() {
+		return game;
+	}
+	public Board(Block[] allBlocks, Block selected, int size, int finishx, int finishy, Game game)
+	{
+		super();
+		this.game = game;
+		this.size = size;
+		this.selected = selected;
+		this.setLayout(new GridBagLayout());
+		this.setSize(300, 300);
+		this.setBackground(Color.GREEN);
+		this.allBlocks = new Block[allBlocks.length];
+		for (int i = 0; i < allBlocks.length; i++) 
+		{
+			this.allBlocks[i] = new Block(allBlocks[i]);
+		}
+		CreateFrame(this.size, this.allBlocks);
+		AddBlocks(this.allBlocks);
+		//this.setBorder(BorderFactory.createTitledBorder(
+		//       BorderFactory.createEtchedBorder(), "Board"));
+		this.setVisible(true);
+	}
+	
+	
+	
+	
+	//-2 מסמל מסגרת
+	//-1 פנוי
+	//כל מספר אחר מסמן תפוס ע"י מספר שמייצג את מספר הבלוק במערך
+	private void CreateFrame(int size , Block[] allBlocks)
+	{
+		this.arrBoard = new int[size+2][size+2];
+		for(int i = 0; i < size+2; i++)
+		{
+			for (int j = 0; j < size+2; j++)
+			{
+				if(i == 0 || j== 0 || i == size+1 || j == size+1)
+					this.arrBoard[i][j] = -2;
+				else
+					this.arrBoard[i][j] = -1;
+			}
+		}
+		for(int i = 0; i < this.allBlocks.length; i++)
+		{
+			Block tmp = this.allBlocks[i];
+			this.arrBoard[tmp.getX()][tmp.getY()] = i;
+			for(int j = 1; j < tmp.getLength(); j++)
+			{
+				if(tmp.getDir().equals("Horizontal"))
+				{
+					this.arrBoard[tmp.getX() + j][tmp.getY()] = i;
+				}
+				else
+				{
+					this.arrBoard[tmp.getX()][tmp.getY() + j] = i;
+				}
+			}
+		}
+		PrintArray(this.arrBoard);
+		
+	}
+	private void PrintArray(int[][] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			for (int j = 0; j < arr[0].length; j++)
+			{
+				System.out.print(arr[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+	private void AddBlocks(Block[] allBlocks)
+	{
+		GridBagConstraints gbc;
+		for (int i = 1; i < this.size+1; i++) 
+		{
+			for (int j = 1; j < this.size+1; j++) 
+			{
+				if(this.arrBoard[i][j] == -1)
+				{
+					gbc = new GridBagConstraints();
+					JLabel free = new JLabel();
+					free.setIcon(new ImageIcon("Images/FreeSpace.jpg"));
+					gbc.gridx = i;
+					gbc.gridy = j;
+					this.add(free, gbc);
+				}
+			}
+		}
+			for (int i = 0; i < this.allBlocks.length; i++)
+			{
+					Block tmp = allBlocks[i];
+					System.out.println("i:"+i+" x:"+ allBlocks[i].getX()+" y:"+ allBlocks[i].getY() );
+					gbc = new GridBagConstraints();
+					JLabel toAdd = new Block(tmp);
+					toAdd.setIcon(new ImageIcon("Images/FreeSpace.png"));
+
+					if(tmp.getDir().equals("Horizontal"))
+					{
+						gbc.fill = GridBagConstraints.HORIZONTAL;
+						gbc.gridwidth = tmp.getLength();
+
+					}
+					else
+					{
+						gbc.fill = GridBagConstraints.VERTICAL;
+						gbc.gridheight = tmp.getLength();
+						
+					}
+					if(tmp.getTarget())
+					{
+						toAdd.setIcon(new ImageIcon("Images//BlockTarget.png"));
+						//toAdd.setBackground(Color.RED);
+					}
+					else
+					{
+						toAdd.setIcon(new ImageIcon("Images//Block.png"));
+						//toAdd.setBackground(Color.gray);
+					}
+					//if(tmp.equals(this.selected))
+					//{
+					//	toAdd.setIcon(new ImageIcon("Images//BlockSelected.png"));
+					    //toAdd.setBackground(Color.black);
+					//}
+					toAdd.setBorder(BorderFactory.createLineBorder(Color.black));
+					toAdd.addMouseListener(this);
+					toAdd.addKeyListener(this);
+					System.out.println("i:"+i+" x:"+ tmp.getX()+" y:"+tmp.getY() );
+
+					gbc.gridx = tmp.getX();				
+					gbc.gridy = tmp.getY();
+
+					System.out.println("i:"+i+" x:"+ gbc.gridx+" y:"+gbc.gridy );
+					
+					this.add(toAdd, gbc);
+				}		
+		/*for (int i = 0; i < allBlocks.length; i++) 
+		{
+			Block tmp = new Block(allBlocks[i]);
+			gbc = new GridBagConstraints();
+			JLabel toAdd = new Block(tmp);
+			toAdd.setText("bb");
+			//toAdd.setIcon(new ImageIcon("Images/Block.jpg"));
+			gbc.gridx = tmp.getX() +1;
+			gbc.gridy = tmp.getY() + 1;
+			if(tmp.getDir().equals("Horizontal"))
+			{
+				gbc.fill = GridBagConstraints.HORIZONTAL;
+				gbc.gridwidth = tmp.getLength();
+			}
+			else
+			{
+				gbc.fill = GridBagConstraints.VERTICAL;
+				gbc.gridheight = tmp.getLength();
+			}
+			if(tmp.getTarget())
+			{
+				//toAdd.setIcon(new ImageIcon("Images//BlockTarget.jpg"));
+				toAdd.setBackground(Color.RED);
+			}
+			else
+			{
+				//toAdd.setIcon(new ImageIcon("Images//Block.jpg"));
+				toAdd.setBackground(Color.gray);
+			}
+			if(tmp.equals(this.selected))
+			{
+				//toAdd.setIcon(new ImageIcon("Images//BlockSelected.png"));
+			    toAdd.setBackground(Color.black);
+			}
+			toAdd.setBorder(BorderFactory.createLineBorder(Color.white));
+			toAdd.addMouseListener(this);
+			this.add(toAdd, gbc);*/
+		}
+		
+	public void ChangeColor()
+	{
+		if(this.selected.getTarget())
+		{
+			this.selected.setBackground(Color.red);
+		}
+		else
+		{
+			this.selected.setBackground(Color.gray);
+		}
+		
+	}
+		public int getBoardSize() {
+			return size;
+		}
+		public Block getSelected() {
+			return selected;
+		}
+
+		public void setSelected(Block selected) {
+			this.selected = selected;
+		}
+
+		public Block[] getAllBlocks() 
+		{
+			Block[] ans = new Block[this.allBlocks.length];
+			for (int i = 0; i < ans.length; i++) 
+			{
+				ans[i] = new Block(this.allBlocks[i]);
+			}
+			return ans;
+		}
+
+		public void setAllBlocks(Block[] allBlocks) {
+			this.allBlocks = allBlocks;
+		}
+
+		public int[][] getArrBoard() {
+			return arrBoard;
+		}
+
+		public void setArrBoard(int[][] arrBoard)
+		{
+			this.arrBoard = arrBoard;
+		}
+		//public void paint(Graphics g)
+		//{
+		//	Image offIm = createImage(getSize().width, getSize().height);
+			//Graphics offGr = offIm.getGraphics();
+			/////////////////////////////////
+			////////////////////////////////
+			///////////////////////////////
+		//}
+
+	public void MoveBlock(String dir)
+	{
+		int x= this.selected.getX();
+		int y = this.selected.getY();
+		int blockNum = this.arrBoard[x][y];
+		int length = this.selected.getLength();
+		String dirHorOrVer = this.selected.getDir();
+		switch(dir)
+		{
+		case "right" :
+			if(dirHorOrVer.equals("Vertical")) 
+			{
+				//the block is not horizontal
+			}
+			if(arrBoard[x + length][y] == -1 )//free place to go
+			{
+				this.selected.moveRight();
+				arrBoard[x + length][y] = blockNum;
+				arrBoard[x][y] = -1;
+				
+			}
+		case "left" :
+			if(dirHorOrVer.equals("Vertical")) 
+			{
+				//the block is not horizontal
+			}
+			if(arrBoard[x-1][y] == -1 )//free place to go
+			{
+				this.selected.moveLeft();
+				arrBoard[x-1][y] = blockNum;
+				arrBoard[x+ length-1][y] = -1;
+			}
+		case "up" :
+			if(dirHorOrVer.equals("Horizontal")) 
+			{
+				//the block is not vertical
+			}
+			if(arrBoard[x][y + 1 ] == -1 )//free place to go
+			{
+				this.selected.moveUp();
+				arrBoard[x][y +1 ] = blockNum;
+				arrBoard[x][y - length  + 1] = -1; 
+			}
+		case "down" :
+			if(dirHorOrVer.equals("Horizontal")) 
+			{
+				//the block is not vertical
+			}
+			if(arrBoard[x][y - length] == -1 )//free place to go
+			{
+				this.selected.moveDown();
+				arrBoard[x][y - length] = blockNum;
+				arrBoard[x][y] = -1;
+			}
+		}
+		if(this.arrBoard[finishx][finishy] > -1)
+		{
+			this.game.GameFinished();
+		}
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+		if(e.getSource() instanceof Block)
+		{
+			((Block)e.getSource()).setSelected(true);
+			this.selected = (Block)e.getSource();
+		}
+		//this.setFocusable(true);
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+				this.setFocusable(true);
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) 
+	{
+		 int keyCode = e.getKeyCode();
+		 String dirToMove = "";
+		    switch( keyCode ) 
+		    { 
+		        case KeyEvent.VK_UP:
+		        	dirToMove = "up";
+		            break;
+		        case KeyEvent.VK_DOWN:
+		        	dirToMove="down";
+		        	break;
+		        case KeyEvent.VK_LEFT:
+		        	dirToMove="left";
+		            break;
+		        case KeyEvent.VK_RIGHT :
+		        	dirToMove="right";
+		            break;
+		    }
+		    MoveBlock(dirToMove);
+		    repaint();
+		    
+		    
+		    //Check if the game is finished
+		    if(this.selected.getTarget())
+		    {
+		    	//Target Block is always horizontal and finishy and y are the same.
+		    	if(this.selected.getX() + this.selected.getLength() >= this.finishx)
+		    	{
+		    		//game finished
+		    	}
+		    }
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
