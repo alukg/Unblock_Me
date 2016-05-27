3fb968eb176cd8f760d6079488498a865547ba8a
alukg <guyaluk@gmail.com>
2016-05-27 14:56:49 +0300

    controller finish and start game

--- src/GameMenu/Controller.java
+++ src/GameMenu/Controller.java
@@ -5,8 +5,6 @@ import GameComponents.Game;
 import GameComponents.Level;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Vector;
@@ -21,6 +19,7 @@ public class Controller extends JFrame {
     private GameEditor gameEditor;
     private Game gameWindow;
     private Vector<Level> levelsDB;
+    private Integer openLevel;
 
     public Controller(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -62,10 +61,60 @@ public class Controller extends JFrame {
         DataBase.Serializer.serialize(levelsDB);
     }
 
-    public void gameFinished(String time){
-        levelsWindow.addLevelChoosePanel();
-        cards.show(getContentPane(),"Select level");
-        gameWindow = null;
+    public void gameFinished(String thisTime, String bestTime){
+        String[] thisTimeSplit = thisTime.split(" \\: ");
+        String[] bestTimeSplit = bestTime.split(" \\: ");
+        String newBestTime;
+        if(Integer.parseInt(thisTimeSplit[0])>Integer.parseInt(bestTimeSplit[0]))
+           newBestTime=thisTime;
+        else if(Integer.parseInt(thisTimeSplit[0])<Integer.parseInt(bestTimeSplit[0]))
+            newBestTime=bestTime;
+        else{
+            if(Integer.parseInt(thisTimeSplit[1])>Integer.parseInt(bestTimeSplit[1]))
+                newBestTime=thisTime;
+            else if(Integer.parseInt(thisTimeSplit[1])<Integer.parseInt(bestTimeSplit[1]))
+                newBestTime=bestTime;
+            else
+                newBestTime=thisTime;
+        }
+        levelsDB.elementAt(openLevel).bestTime=newBestTime;
+        boolean stop = false;
+        while(!stop){
+            String[] options = { "Back to Main menu", "Select another level", "Next level" };
+            JPanel panel = new JPanel();
+            String dialogString;
+            if(newBestTime.equals(thisTime))
+                dialogString = "Congradulations! New record!\nBest time: "+newBestTime+"\nYour time: "+thisTime;
+            else
+                dialogString = "Congradulations! Successfully complete a level\nBest time: "+newBestTime+"\nYour time: "+thisTime;
+            panel.add(new JLabel(dialogString), BorderLayout.CENTER);
+            int selected = JOptionPane.showOptionDialog(controller,panel,"", JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+            if(selected == 0){ //back to main menu
+                stop = true;
+                openLevel=null;
+                cards.show(getContentPane(),"Home");
+                gameWindow = null;
+            }
+            else if(selected == 1){ //select another level
+                stop=true;
+                openLevel=null;
+                levelsWindow.addLevelChoosePanel();
+                cards.show(getContentPane(),"Select level");
+                gameWindow = null;
+            }
+            else{ //next level
+                if(openLevel==levelsDB.size()-1){
+                    JOptionPane.showOptionDialog(controller,"There is no next level","", JOptionPane.OK_OPTION,
+                            JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+                    continue;
+                }
+                else{
+                    stop = true;
+                    newGame(openLevel+1);
+                }
+            }
+        }
     }
 
     public class menuPress implements ActionListener {
@@ -103,35 +152,40 @@ public class Controller extends JFrame {
 
     public class gamePress implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            if(gameWindow!=null){
-                getContentPane().remove(gameWindow);
-            }
-            int levelSlot = Integer.parseInt(e.getActionCommand());
-            Level level = levelsDB.elementAt(levelSlot);
-            Block[] blocks = new Block[level.blocks.length];
-            for(int i=0;i<blocks.length;i++){
-                int x = (int)level.blocks[i][0];
-                int y = (int)level.blocks[i][1];
-                int length = (int)level.blocks[i][2];
-                String dir = (String)level.blocks[i][3];
-                Boolean target = (Boolean)level.blocks[i][4];
-                blocks[i] = new Block(x,y,length,dir,target);
-            }
-            gameWindow = new Game(blocks,level.bestTime);
-            getContentPane().add(gameWindow, "Game");
-            cards.show(getContentPane(), "Game");
+            newGame(Integer.parseInt(e.getActionCommand()));
         }
+    }
+
+    public void newGame(int levelSlot){
+        if(gameWindow!=null){
+            getContentPane().remove(gameWindow);
+        }
+        Level level = levelsDB.elementAt(levelSlot);
+        Block[] blocks = new Block[level.blocks.length];
+        for(int i=0;i<blocks.length;i++){
+            int x = (int)level.blocks[i][0];
+            int y = (int)level.blocks[i][1];
+            int length = (int)level.blocks[i][2];
+            String dir = (String)level.blocks[i][3];
+            Boolean target = (Boolean)level.blocks[i][4];
+            blocks[i] = new Block(x,y,length,dir,target);
+        }
+        gameWindow = new Game(controller,blocks,level.bestTime);
+        openLevel = levelSlot;
+        getContentPane().add(gameWindow, "Game");
+        cards.show(getContentPane(), "Game");
     }
 
     public class removeLevelPress implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this level?","Warning",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
-            if(dialogResult == JOptionPane.YES_OPTION){
-                int dialogResult2 = JOptionPane.showConfirmDialog(null, "Really sure? that you won't cry after this","Warning",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
-                if(dialogResult2 == JOptionPane.YES_OPTION){
-                    levelsDB.removeElementAt(Integer.parseInt(e.getActionCommand()));
-                    editorWindow.addLevelChoosePanel();
-                }
+            String[] options = { "Confirm", "Cancel" };
+            JPanel panel = new JPanel();
+            panel.add(new JLabel("Are you sure you want to delete this level?"), BorderLayout.CENTER);
+            int selected = JOptionPane.showOptionDialog(controller,panel,"Confirmation", JOptionPane.YES_NO_OPTION,
+                    JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+            if(selected == JOptionPane.YES_OPTION){
+                levelsDB.removeElementAt(Integer.parseInt(e.getActionCommand()));
+                editorWindow.addLevelChoosePanel();
             }
         }
     }
